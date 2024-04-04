d7a4d5ac418149aa27305d03b7470849c1e3ecca
Li Feng <maplestarplay@gmail.com>
2024-04-04 13:51:27 +0800

    feat: change orm from exposed to ktorm

--- src/main/kotlin/com/example/DB.kt
+++ src/main/kotlin/com/example/DB.kt
@@ -1,27 +1,21 @@
 package com.example
 
-import com.example.entity.Articles
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.config.*
-import kotlinx.coroutines.Dispatchers
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.ktorm.database.Database
 import java.io.File
 
 object DB {
-    fun init(config: ApplicationConfig) {
+    lateinit var  database: Database
+
+    fun init(config: ApplicationConfig)  {
         val driverClassName = config.property("storage.driverClassName").getString()
         val jdbcURL = config.property("storage.jdbcURL").getString() +
                 (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
                     File(it).canonicalFile.absolutePath
                 } ?: "")
-        val database = Database.connect(createHikariDataSource(url = jdbcURL, driver = driverClassName))
-        transaction(database) {
-            SchemaUtils.create(Articles)
-        }
+        database = Database.connect(createHikariDataSource(url = jdbcURL, driver = driverClassName))
     }
     private fun createHikariDataSource(
         url: String,
@@ -30,11 +24,9 @@ object DB {
         driverClassName = driver
         jdbcUrl = url
         maximumPoolSize = 10
-        isAutoCommit = false
+        isAutoCommit = true
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         validate()
         username = "postgres"
     })
-    suspend fun <T> dbQuery(block: suspend () -> T): T =
-        newSuspendedTransaction(Dispatchers.IO) { block() }
 }
